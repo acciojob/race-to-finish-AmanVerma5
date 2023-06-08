4fe1925e50295e1de14523eedc73a9c00380fcf7
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-08 12:37:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,42 +8,45 @@ function getRandomTime(){
     return randomTime;
 }
 
-let prom1 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, getRandomTime());
-})
-let prom2 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, getRandomTime());
-})
-let prom3 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, getRandomTime());
-})
-let prom4 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, getRandomTime());	
-})
-let prom5 = new Promise((resolve) => {
-  setTimeout(() => {
-    resolve();
-  }, getRandomTime());
-})
-window.promises=[prom1,prom2,prom3,prom4,prom5];
+const id = document.getElementById("output")
 
-let x=Promise.any([prom1,prom2,prom3,prom4,prom5]);
 
-let id=document.getElementById("output");
+let prom1 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data1");
+  },getRandomTime())
+});
 
-x.then(()=>{
-    id.innerText="output";
-})
+let prom2 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data2")
+  },getRandomTime())
+});
+let prom3 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data3")
+  },getRandomTime())
+});
+let prom4 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data4")
+  },getRandomTime())
+});
+let prom5 = new Promise((resolve,reject) =>{
+  setTimeout(() =>{
+    resolve("data5")
+  },getRandomTime())
+});
+window.promises = [prom1, prom2, prom3, prom4, prom5];
+let x = Promise.any([prom1, prom2, prom3, prom4, prom5])
 
-x.catch((error)=>{
-    console.log(error);
-})
+x.then((data) =>{
 
+  // console.log(data);
+  id.innerText = data;
+
+});
+
+x.catch((error) =>{
+  console.log("error :", error)
+});
