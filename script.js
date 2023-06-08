f14d08b68158720db43f5f584f092a9a77f1efef
AmanVerma5 <118344057+AmanVerma5@users.noreply.github.com>
2023-06-08 12:16:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+function getRandomTIme(){
+    let sec=Math.random()*4+1;
+    let randomTIme=sec*1000;
+    return randomTIme;
+}
+
+let prom1=new Promise((resolve)=>{
+    resolve();
+},getRandomTIme);
+let prom2=new Promise((resolve)=>{
+    resolve();
+},getRandomTIme);
+let prom3=new Promise((resolve)=>{
+    resolve();
+},getRandomTIme);
+let prom4=new Promise((resolve)=>{
+    resolve();
+},getRandomTIme);
+let prom5=new Promise((resolve)=>{
+    resolve();
+},getRandomTIme);
+
+window.promises=[prom1,prom2,prom3,prom4,prom5];
+
+let x=Promise.any([prom1,prom2,prom3,prom4,prom5]);
+
+let id=document.getElementById("output");
+
+x.then(()=>{
+    id.innerText="output";
+})
+
+x.catch((error)=>{
+    console.log(error);
+})
